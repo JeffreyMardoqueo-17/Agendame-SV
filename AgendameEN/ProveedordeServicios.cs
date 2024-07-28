01f7d23ef1ca8fe834816d75f14e496778487692
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-28 12:18:29 -0600

    Clases de registro de los 3 tipos de Usuario 🎶✨

---
+++ AgendameEN/ProveedordeServicios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendameEN
+{
+    public class ProveedordeServicios
+    {
+        public int Id { get; set; }
+        public Usuario ?Usuario { get; set; }
+        public int IdDireccionesServicios { get; set; }
+        public string? Horario { get; set; }
+        public int IdCategoria { get; set; }
+        public List<Servicio>? Servicios { get; set; } ///porque el usuario elegira todos los servicios que estan dentro de la categoria que eleija
+    }
+}
