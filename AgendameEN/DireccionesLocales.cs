f19a1aece17ac87f9800bd0467ec34f015607801
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-29 20:16:07 -0600

    Clases arregladas de los locales

--- AgendameEN/DireccionesLocales.cs
+++ AgendameEN/DireccionesLocales.cs
@@ -9,10 +9,10 @@ namespace AgendameEN
     public class DireccionesLocales
     {
         public int Id { get; set; }
-        public string ? Calle { get; set;}
-        public string ? Ciudad { get; set;}
-        public string ? Departamento { get; set;}
-        public string ? Latitud { get; set;}
-        public string ? Longitud { get; set;}
+        public string? Calle { get; set;}
+        public string? Ciudad { get; set;}
+        public string? Departamento { get; set;}
+        public string? Latitud { get; set;}
+        public string? Longitud { get; set;}
     }
 }
